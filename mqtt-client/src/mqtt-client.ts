71d24dc3cb8e7668b2a9eb48713310d6752f2183
Vinicius Cestari <124843824+ViniciusCestarii@users.noreply.github.com>
2025-06-21 10:49:34 -0300

    Update mqtt-client.ts

--- mqtt-client/src/mqtt-client.ts
+++ mqtt-client/src/mqtt-client.ts
@@ -6,7 +6,7 @@ if (!process.env.MQTT_SERVER_URL) {
   throw new Error("MQTT_SERVER_URL not found on env!")
 }
 
-const client = mqtt.connect("mqtt://test.mosquitto.org");
+const client = mqtt.connect(process.env.MQTT_SERVER_URL);
 
 client.on("connect", () => {
   console.log(`Connected to ${process.env.MQTT_SERVER_URL}`)
